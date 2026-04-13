8033c160759ade517864380e6769009e523855a8
skshiva31112 <skshiva3112@gmail.com>
2026-04-13 09:31:36 +0530

    This is my Third Commit

---
+++ src/test/java/demo/Practice2.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class Practice2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome....");
+		System.out.println("Shiva Kumar R");
+	}
+
+}
